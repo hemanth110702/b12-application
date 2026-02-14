b2d477ffcd2a0a8633b63b4a2b0dabd1e247a904
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2026-02-14 22:59:58 +0530

    test: added debugging statements
    added some more debugging checkpoints to check execution of code

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ from datetime import datetime, timezone
 
 timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
 
+print("Checkpoint-1")
+
 payload = {
   "action_run_link": "https://github.com/hemanth110702/b12-application/actions/runs/22021144971",
   "email": "mamidihemanthkumar2002@gmail.com",
@@ -15,11 +17,17 @@ payload = {
   "timestamp": timestamp,
 }
 
+print("Checkpoint-2")
+
 body = json.dumps(payload, separators=(",", ":"), sort_keys=True)
+
+print("Checkpoint-3")
 
 secret = b"hello-there-from-b12"
 signature = hmac.new(secret, body.encode("utf-8"), hashlib.sha256).hexdigest()
 header_signature = f"sha256={signature}"
+
+print("Checkpoint-4")
 
 response = requests.post(
   "https://b12.io/apply/submission",
@@ -29,6 +37,8 @@ response = requests.post(
     "X-Signature-256": header_signature
   }
 )
+
+print("Checkpoint-5")
 
 print("STATUS:", response.status_code)
 print("RESPONSE:", response.text)
